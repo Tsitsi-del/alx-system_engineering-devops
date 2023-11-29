2322b5914745ad06aba9f16f9aaeb85053f2bfbb
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-11-29 12:40:55 +0200

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,54 @@
-#!/usr/bin/python3
-"""RESST API for todo lists of employees"""
+#!/usr/bin/pyhton3
+"""API for todo lists of employees"""
+
 
 import requests
 import sys
 
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+def get_employee_info(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = f"{base_url}/{employee_id}"
 
     response = requests.get(url)
-    employeeName = response.json().get('name')
+    response.raise_for_status()
+    return response.json().get('name')
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
+
+def get_completed_tasks(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = f"{base_url}/{employee_id}/todos"
+
+    response = requests.get(url)
+    response.raise_for_status()
     tasks = response.json()
-    done = 0
-    done_tasks = []
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+    done_tasks = [task for task in tasks if task.get('completed')]
+    return tasks, done_tasks
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
 
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    try:
+        employee_name = get_employee_info(employee_id)
+        tasks, done_tasks = get_completed_tasks(employee_id)
+
+        total_tasks = len(tasks)
+        done_task_count = len(done_tasks)
+
+        print(f"Employee {employee_name} is done with tasks "
+              f"({done_task_count}/{total_tasks}):")
+        for task in done_tasks:
+            print(f"\t{task.get('title')}")
+
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
