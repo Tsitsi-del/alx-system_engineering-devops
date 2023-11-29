721a5b1263855e7309ccc16db953b09cd1f875b4
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-11-29 12:03:59 +0200

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-Rest API for todo lists of employees
-"""
+"""Rest API for todo lists of employees"""
 
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    employee_Id = sys.argv[1]
+    employeeId = sys.argv[1]
     baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + emplo_Id
+    url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
-    emplo_name = response.json().get("name")
+    employeeName = response.json().get("name")
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
@@ -27,7 +25,7 @@ if __name__ == "__main__":
             done += 1
 
     print("Employee {} is done with tasks({}/ {}):"
-          .formart(emplo_name, done, len(tasks)))
+          .formart(employeeName, done, len(tasks)))
 
     for t in tasks_done:
         print("\t {}".format(t.get('title")))
